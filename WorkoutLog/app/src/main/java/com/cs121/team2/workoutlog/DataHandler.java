53a59b7106d91a75d5b6ced78190071ed088ba02
sjackson212 <Tripp@Samuels-MacBook-Air.local>
2014-10-04 15:13:57 -0700

    Created DataHandler Class
    Entered initial needed methods, tried to implement singleton form
    correctly.

---
+++ WorkoutLog/app/src/main/java/com/cs121/team2/workoutlog/DataHandler.java
@@ -0,0 +1,46 @@
+package com.cs121.team2.workoutlog;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Sam J on 10/4/14.
+ */
+
+/*
+ * TODO:
+ * add variables needed for this class
+ * finish constructor
+ * fill in the methods
+ * think of a better name for "Log" because that is a Java object already
+ * change return type of readData() and convertFromLogs to something proper
+ */
+public class DataHandler {
+    public static DataHandler _dh; //the DataHandler instance that will be constructed and kept
+    private DataHandler() { //this is a singleton class, so this is kept private
+
+    }
+    public synchronized static DataHandler GetDataHandler() { //used to make/get the DH
+        if (_dh == null) { //does the DH already exist?
+            _dh = new DataHandler(); //if not, create a new one
+        }
+        return _dh; //if so, just return the DH that is already instantiated
+    }
+    public synchronized ArrayList<Log> getLogs() { //sends the ArrayList of Logs to LLAdapter
+
+    }
+    private synchronized void readData() { //reads the data file from internal storage
+
+    }
+    private synchronized void writeData() { //writes the data into internal storage
+
+    }
+    private synchronized ArrayList<Log> convertToLogs() { //converts the data file into Log AList
+
+    }
+    private synchronized void convertFromLogs() { //converts Log AList into usable file for storage
+
+    }
+    public synchronized void addLog(Log toAdd) { //appends a new log to the Log AList
+
+    }
+}
