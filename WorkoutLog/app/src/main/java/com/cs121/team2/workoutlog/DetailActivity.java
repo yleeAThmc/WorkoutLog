5fe44478261188e80d8e9e6a707c80ef3e7f51a5
clairerosburg <clairerosburg@aol.com>
2014-11-02 22:15:20 -0800

    Poop
    Poopoopoopoopoopoopoopoop

--- WorkoutLog/app/src/main/java/com/cs121/team2/workoutlog/DetailActivity.java
+++ WorkoutLog/app/src/main/java/com/cs121/team2/workoutlog/DetailActivity.java
@@ -3,6 +3,8 @@ package com.cs121.team2.workoutlog;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 /**
@@ -31,5 +33,33 @@ public class DetailActivity extends Activity {
 
         //set the text for the TextView
         textView.setText(myLog.toString());
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.wologlist, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        if (id == R.id.add_WOlog) {
+
+            Intent newEntryIntent = new Intent(this, EntryActivity.class);
+            // start the next Activity the prepared Intent
+            startActivity(newEntryIntent);
+
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
