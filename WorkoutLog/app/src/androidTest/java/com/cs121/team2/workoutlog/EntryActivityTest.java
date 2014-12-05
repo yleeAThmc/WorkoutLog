437389319de41a19179a28a21d20fd490a4bfcfe
Kelly Lee <ylee@hmc.edu>
2014-12-05 09:33:20 -0800

    EntryAct Test + minor UI change + exercise names fixed
    EntryActivity Testing
    DetailActivity Buttons UI
    Got rid of settings menu
    Actual exercise names

--- WorkoutLog/app/src/androidTest/java/com/cs121/team2/workoutlog/EntryActivityTest.java
+++ WorkoutLog/app/src/androidTest/java/com/cs121/team2/workoutlog/EntryActivityTest.java
@@ -6,7 +6,7 @@ package com.cs121.team2.workoutlog;
  */
 
 
-//TODO: Kelly is responsible for this class;
+//Kelly is responsible for this class;
 // don't worry about spending too much time on this--maybe an hour, at most 2--to get basic picking and
 //choosing set/checking values selected and stored. Using the android activity tutorial
 //I have linked in basecamp is the best bet. I expect because this is our most complex class that it'll
@@ -15,6 +15,7 @@ package com.cs121.team2.workoutlog;
 import android.content.Intent;
 import android.test.ActivityInstrumentationTestCase2;
 import android.test.ActivityUnitTestCase;
+import android.test.UiThreadTest;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.DatePicker;
@@ -28,13 +29,6 @@ public class EntryActivityTest extends
         ActivityInstrumentationTestCase2 {
 
     private EntryActivity mActivity;
-    private Button btn1;
-    private SeekBar moodBar;
-    private AutoCompleteTextView mAutoCardio;
-    private AutoCompleteTextView mAutoStrength;
-    private WOLog myLog;
-    private DatePicker mDate;
-    private TimePicker mTime;
 
     public EntryActivityTest() {
         super(EntryActivity.class);
@@ -44,42 +38,134 @@ public class EntryActivityTest extends
     @Override
     public void setUp() throws Exception {
         super.setUp();
+        //setActivityInitialTouchMode(false);
         mActivity = (EntryActivity) getActivity();
-        System.out.println("MACTIVITTY: " + mActivity);
-        //startActivity(new Intent(getInstrumentation().getTargetContext(), EntryActivity.class), null, null);
-        btn1 = (Button) mActivity.findViewById(R.id.entry_cardio);
-        mAutoStrength = (AutoCompleteTextView) mActivity.findViewById(R.id.strength_actv);
-        mAutoCardio = (AutoCompleteTextView) mActivity.findViewById(R.id.cardio_actv);
-        moodBar = (SeekBar) mActivity.findViewById(R.id.mood);
-        myLog = new WOLog();
-        mDate = (DatePicker) mActivity.findViewById(R.id.date);
-        mTime = (TimePicker) mActivity.findViewById(R.id.time);
-        ImageButton miBtn = (ImageButton) mActivity.findViewById(R.id.miBtn);
-        ImageButton mBtn = (ImageButton) mActivity.findViewById(R.id.mBtn);
-        ImageButton kmBtn = (ImageButton) mActivity.findViewById(R.id.kmBtn);
-        ImageButton lbBtn = (ImageButton) mActivity.findViewById(R.id.lbBtn);
-        ImageButton kgBtn = (ImageButton) mActivity.findViewById(R.id.kgBtn);
-        ImageButton distButton = (ImageButton) mActivity.findViewById(R.id.custom_dist_btn);
-        ImageButton timeButton = (ImageButton) mActivity.findViewById(R.id.custom_dur_btn);
-        ImageButton weightButton = (ImageButton) mActivity.findViewById(R.id.custom_wgt_btn);
-        ImageButton setRepButton = (ImageButton) mActivity.findViewById(R.id.custom_setrep_btn);
-        EditText customDist = (EditText) mActivity.findViewById(R.id.custom_dist);
-        EditText customHour = (EditText) mActivity.findViewById(R.id.custom_hour);
-        EditText customMinute = (EditText) mActivity.findViewById(R.id.custom_minute);
-        EditText customWeight = (EditText) mActivity.findViewById(R.id.custom_wgt);
-        EditText customSets = (EditText) mActivity.findViewById(R.id.custom_sets);
-        EditText customReps = (EditText) mActivity.findViewById(R.id.custom_reps);
-
     }
 
 
-    //
-    public void testFirstJokeExample() {
-        String blah = "ssss";
-        assertNotNull(blah);
+    // check if it the wolog gets the right type and subtype assigned
+    @UiThreadTest
+    public void testCardioTime() {
+        Button cardioBtn = (Button) mActivity.findViewById(R.id.entry_cardio);
+        cardioBtn.performClick();
+        Button timeBtn = (Button) mActivity.findViewById(R.id.entry_time);
+        timeBtn.performClick();
+        EditText memo = (EditText) mActivity.findViewById(R.id.memo);
+        assertNotNull(memo);
 
-//        buttonId = getActivity().R.id.view_list_button;
-         assertNotNull(btn1);
+        // Check if type / subtypes are assigned correctly
+        assertEquals(mActivity._type, WOLog.TYPE_CARDIO);
+        assertEquals(mActivity._subType, WOLog.SUBTYPE_TIME_BODY);
+
+        Button nextBtn = (Button) mActivity.findViewById(R.id.common_next);
+        nextBtn.performClick();
+
+        // Check if the correct layout is displayed
+        assertNotNull(mActivity.findViewById(R.id.cardio_dist));
+        assertNull(mActivity.findViewById(R.id.strength_weight));
+        assertNull(mActivity.findViewById(R.id.custom_dist));
+
+        // Check if the correct default unit button is selected
+        assertEquals(mActivity._cardioUnit, WOLog.UNIT_MI_LB);
+    }
+
+    @UiThreadTest
+    public void testCardioDist() {
+        Button cardioBtn = (Button) mActivity.findViewById(R.id.entry_cardio);
+        cardioBtn.performClick();
+        Button distBtn = (Button) mActivity.findViewById(R.id.entry_dist);
+        distBtn.performClick();
+        EditText memo = (EditText) mActivity.findViewById(R.id.memo);
+        assertNotNull(memo);
+
+        // Check if type / subtypes are assigned correctly
+        assertEquals(mActivity._type, WOLog.TYPE_CARDIO);
+        assertEquals(mActivity._subType, WOLog.SUBTYPE_DIST_WEIGHTS);
+
+        Button nextBtn = (Button) mActivity.findViewById(R.id.common_next);
+        nextBtn.performClick();
+
+        // Check if the correct layout is displayed
+        assertNotNull(mActivity.findViewById(R.id.cardio_dist));
+        assertNull(mActivity.findViewById(R.id.strength_weight));
+        assertNull(mActivity.findViewById(R.id.custom_dist));
+
+        // Check if the correct default unit button is selected
+        assertEquals(mActivity._cardioUnit, WOLog.UNIT_MI_LB);
+    }
+
+    @UiThreadTest
+    public void testStrengthWeights() {
+        Button strengthBtn = (Button) mActivity.findViewById(R.id.entry_strength);
+        strengthBtn.performClick();
+        Button wgtBtn = (Button) mActivity.findViewById(R.id.entry_wgt);
+        wgtBtn.performClick();
+        EditText memo = (EditText) mActivity.findViewById(R.id.memo);
+        assertNotNull(memo);
+
+        // check if type / subtypes are assigned correctly
+        assertEquals(mActivity._type, WOLog.TYPE_STRENGTH);
+        assertEquals(mActivity._subType, WOLog.SUBTYPE_DIST_WEIGHTS);
+
+        Button nextBtn = (Button) mActivity.findViewById(R.id.common_next);
+        nextBtn.performClick();
+
+        // Check if the correct layout is displayed
+        assertNull(mActivity.findViewById(R.id.cardio_dist));
+        assertNotNull(mActivity.findViewById(R.id.strength_weight));
+        assertNull(mActivity.findViewById(R.id.custom_dist));
+
+        // Check if the correct default unit button is selected
+        assertEquals(mActivity._strengthUnit, WOLog.UNIT_MI_LB);
+    }
+
+    @UiThreadTest
+    public void testStrengthBodyWeights() {
+        Button strengthBtn = (Button) mActivity.findViewById(R.id.entry_strength);
+        strengthBtn.performClick();
+        Button bodyWgtBtn = (Button) mActivity.findViewById(R.id.entry_body_wgt);
+        bodyWgtBtn.performClick();
+        EditText memo = (EditText) mActivity.findViewById(R.id.memo);
+        assertNotNull(memo);
+
+        // check if type / subtypes are assigned correctly
+        assertEquals(mActivity._type, WOLog.TYPE_STRENGTH);
+        assertEquals(mActivity._subType, WOLog.SUBTYPE_TIME_BODY);
+
+        Button nextBtn = (Button) mActivity.findViewById(R.id.common_next);
+        nextBtn.performClick();
+
+        // Check if the correct layout is displayed
+        assertNull(mActivity.findViewById(R.id.cardio_dist));
+        assertNotNull(mActivity.findViewById(R.id.strength_weight));
+        assertNull(mActivity.findViewById(R.id.custom_dist));
+
+        // Check if the correct default unit button is selected
+        assertEquals(mActivity._strengthUnit, WOLog.UNIT_MI_LB);
+    }
+
+    @UiThreadTest
+    public void testCustom() {
+        Button customBtn = (Button) mActivity.findViewById(R.id.entry_custom);
+        customBtn.performClick();
+        EditText memo = (EditText) mActivity.findViewById(R.id.memo);
+        assertNotNull(memo);
+
+        // check if type / subtypes are assigned correctly
+        assertEquals(mActivity._type, WOLog.TYPE_CUSTOM);
+        assertEquals(mActivity._subType, WOLog.SUBTYPE_NONE);
+
+        Button nextBtn = (Button) mActivity.findViewById(R.id.common_next);
+        nextBtn.performClick();
+
+        // Check if the correct layout is displayed
+        assertNull(mActivity.findViewById(R.id.cardio_dist));
+        assertNull(mActivity.findViewById(R.id.strength_weight));
+        assertNotNull(mActivity.findViewById(R.id.custom_dist));
+
+        // Check if the correct default unit button is selected
+        assertEquals(mActivity._cardioUnit, WOLog.UNIT_MI_LB);
+        assertEquals(mActivity._strengthUnit, WOLog.UNIT_MI_LB);
     }
 
 }
